0e40a0a8fd035fba907a494e345c90d6e6b35718
Jain <rahul.jain03@sap.com>
2019-09-30 09:38:16 +0530

    README Added

--- app/app.ts
+++ app/app.ts
@@ -3,27 +3,27 @@ import * as Express from "express";
 import * as Mongoose from "mongoose";
 import * as BodyParser from "body-parser";
 import * as Morgan from "morgan";
-import { ProjectRoute, ProjectModel } from "./modules/project";
+import { ProjectRoute } from "./modules/project";
+
+
+const port: string = env.PORT || '3000';
+const mongoURL: string = 'mongodb://localhost:27017/ResourceManagementDB';
+
 
 const app: Express.Application = Express();
-const port: string = env.PORT || '3000';
-
-
 app.use(Morgan('combined'));
 app.use(BodyParser.urlencoded({ extended: false }));
 app.use(BodyParser.json());
 app.get('/', (req, res) => {
-    res.json({"body": "I got it"});
+    res.json({"message": "Welcome to RM!!!"});
 });
 
 
 const projectRoute = new ProjectRoute();
 app.use(ProjectRoute.BasePath, projectRoute.getRouter());
 
-Mongoose.connect('mongodb://localhost:27017/ResourceManagementDB', { useNewUrlParser: true});
+Mongoose.connect(mongoURL, { useNewUrlParser: true});
 const db = Mongoose.connection;
-
-//db.model(ProjectModel.CollectionName);
 
 db.on('error', console.error.bind(console, 'connection: error'));
 db.on('open', () => {
---
+++ app/modules/project/README.md
@@ -0,0 +1,48 @@
+# Project
+
+The Project route is ```/project```.
+
+To create project use following example:
+```
+POST: /project
+Body:
+    {
+        "ID": "Project1",
+        "name": "Project 1",
+        "startDate": "2019-09-30T02:50:25.399Z",
+        "endDate": "2019-09-30T02:50:25.399Z",
+        "changedOn": "2019-09-30T02:50:25.399Z",
+        "createdOn": "2019-09-30T02:50:25.399Z"
+    }
+```
+
+To update project use the following example:
+```
+PUT: /project/Project1
+Body:
+    {
+        "_id": "5d916d91bc4c3748a4c35da6",
+        "ID": "Project1",
+        "name": "Project 1",
+        "startDate": "2019-09-30T02:50:25.399Z",
+        "endDate": "2019-09-30T02:50:25.399Z",
+        "changedOn": "2019-09-30T02:50:25.399Z",
+        "createdOn": "2019-09-30T02:50:25.399Z",
+        "__v": 0
+    }
+```
+
+To delete:
+```
+DELETE: /project/Project1
+```
+
+To get app projects:
+```
+GET: /project
+```
+
+To get a particular project
+```
+GET: /project/Project1
+```
