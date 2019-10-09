fafd6248e5df3fa96a27642e9c6cffc6b9481095
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-09 09:39:54 +0200

    FIxed server test

--- test/app/server.test.ts
+++ test/app/server.test.ts
@@ -1,19 +1,39 @@
-import * as Express from 'express';
-
 import { Server } from '../../app/server';
 
-let server: Server;
-
-beforeAll(() => {
-    server = new Server('8081');
-});
-
 describe('Server test', () => {
-    test('server start', async () => {
-        const spy = jest.spyOn(global.console, 'log');
+    let server: Server;
 
-        server.startApp();
-        expect(spy).toHaveBeenCalled();
-        //expect(console.log.mock.calls[0][0]).toBeUndefined();
+    beforeAll(() => {
+        server = new Server('8081');
+    });
+
+    afterAll(async () => {
+        /**
+         * Since started the server in a test, it's our responsibility to shutdown the server after all tests are run
+         * Otherwise, Jest would still keep some objects in memory and can never exit with '0' code thinking there are other tests to run, unless killed otherwise.
+         */
+        await server.shutdown();
+    });
+
+    it('spins up a new server on 8081 port', async () => {
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        /**
+         * This is called assertion planning.
+         * It ensures that test wait till at least two assertions are made.
+         *
+         * Here it signifies that we are sure that there would be 2 assertions made, if not, mark this test as fail.
+         *
+         * This is important so that if server is not able to start and throws some error,
+         * this test can tell us that there is some problem with the server.
+         */
+        expect.assertions(2);
+
+        const serverMessage = await server.start();
+
+        expect(serverMessage).toBe('Listening to port 8081');
+        expect(consoleSpy).toHaveBeenCalledWith(serverMessage);
+
+        consoleSpy.mockRestore();
     });
 });
