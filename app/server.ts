d2cbb9ebb37fa54b8ec4993e80af359e2c0c581b
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-09 09:39:38 +0200

    added shutdown method

--- app/server.ts
+++ app/server.ts
@@ -1,3 +1,4 @@
+import * as Http from 'http';
 import * as Express from 'express';
 import * as BodyParser from 'body-parser';
 import * as Morgan from 'morgan';
@@ -7,10 +8,12 @@ import { PORT } from './modules/util/config';
 export class Server {
     server: Express.Application;
     port: string;
+    private _server: Http.Server | null;
 
     constructor(port: string = PORT) {
         this.server = Express();
         this.port = port;
+        this._server = null;
     }
 
     attachMiddlewares = (): void => {
@@ -32,4 +35,22 @@ export class Server {
             });
         });
     };
+
+    /**
+     * Shutdown method is very important in an application. Many app developers forget about this little gem.
+     * This gives you control to gracefully shutdown the web server to stop receiving requests.
+     *
+     * It helps to avoid zombie processes and memory leaks.
+     */
+    shutdown() {
+        return new Promise((resolve: () => void) => {
+            if (this._server) {
+                this._server.close(() => {
+                    resolve();
+                });
+            } else {
+                resolve();
+            }
+        });
+    }
 }
