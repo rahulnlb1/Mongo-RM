8c91a2c6ba870514d87904f2222cebf69c6f51ed
Rahul Kashyap <kashyaprahul94@gmail.com>
2019-10-09 10:26:07 +0200

    fixed the project integration test

--- test/integration/project.test.ts
+++ test/integration/project.test.ts
@@ -1,17 +1,30 @@
-import request from 'supertest';
+import axios from 'axios';
+import mongoose from 'mongoose';
 
 import { App } from '../../app';
 
-let app: App;
-beforeAll(() => {
-    app = new App('8081');
-    app.initRoutes();
-    app.initApp();
-});
-
 describe('Project test', () => {
-    test('project get all', () =>
-        request(app)
-            .get('/project')
-            .expect(200));
+    let app: App;
+    let httpClient = axios.create({
+        baseURL: 'http://localhost:8081',
+    });
+
+    beforeAll(async () => {
+        app = new App('8081');
+        app.initRoutes();
+        await app.initApp();
+    });
+
+    afterAll(async () => {
+        await app.getServer().shutdown();
+        await mongoose.connection.close();
+    });
+
+    it('project get all', async () => {
+        expect.assertions(1);
+
+        const response = await httpClient.get('/project');
+
+        expect(response.status).toBe(200);
+    });
 });
